23bf982417d3f084ee01a146da8751498071d70d
dayaswaroopchavali <129617445+dayaswaroopchavali@users.noreply.github.com>
2023-07-23 22:06:32 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(CounterApp());
-}
+void main() => runApp(MyApp());
 
-class CounterApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter App',
+      title: 'Budget Tracker',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CounterPage(),
+      home: MyHomePage(title: 'Budget Tracker'),
     );
   }
 }
 
-class CounterPage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  _CounterPageState createState() => _CounterPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _CounterPageState extends State<CounterPage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> {
+  double totalBalance = 0;
+  List<String> categories = [];
+  List<double> amounts = [];
 
-  void _incrementCounter() {
+  void addEntry(String category, double amount) {
     setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
+      categories.add(category);
+      amounts.add(amount);
+      totalBalance += amount;
     });
   }
 
@@ -41,38 +42,121 @@ class _CounterPageState extends State<CounterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter App'),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Counter Value:',
-              style: TextStyle(fontSize: 20),
+              'Total Balance:',
             ),
             Text(
-              '$_counter',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              '\$${totalBalance.toStringAsFixed(2)}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: categories.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(categories[index]),
+                    trailing:
+                    Text('\$${amounts[index].toStringAsFixed(2)}'),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => NewEntryScreen()),
+          );
+          if (result != null) {
+            addEntry(result['category'], result['amount']);
+          }
+        },
+        tooltip: 'Add Entry',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NewEntryScreen extends StatefulWidget {
+  @override
+  _NewEntryScreenState createState() => _NewEntryScreenState();
+}
+
+class _NewEntryScreenState extends State<NewEntryScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String? _category;
+  double? _amount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Entry'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration:
+                InputDecoration(labelText: 'Category', hintText: 'Food'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a category';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _category = value;
+                },
+              ),
+              TextFormField(
+                decoration:
+                InputDecoration(labelText: 'Amount', hintText: '10.00'),
+                keyboardType:
+                TextInputType.numberWithOptions(decimal: true),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an amount';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Please enter a valid amount';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _amount = double.parse(value!);
+                },
+              ),
+              Padding(
+                padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      Navigator.pop(context, {'category': _category!, 'amount': _amount!});
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+            ],
           ),
-          SizedBox(width: 16),
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-        ],
+        ),
       ),
     );
   }
